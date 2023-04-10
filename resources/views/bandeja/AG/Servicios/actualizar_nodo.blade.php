2afd743bfc554bb14f42a22408f1add9da3f24c6
HmOyola <hmatiasoyola@gmail.com>
2023-04-09 23:00:02 -0300

    Cambio de Encabezados, Footer de cards y estilo de boton

--- resources/views/bandeja/AG/Servicios/actualizar_nodo.blade.php
+++ resources/views/bandeja/AG/Servicios/actualizar_nodo.blade.php
@@ -5,9 +5,9 @@
 @section('ContenidoPrincipal')
 <section id="container" >
     <section id="main-content">
-        <section class="wrapper">
+        <section class="content-wrapper">
             <!-- Inicio Selectores -->
-            <a  href="/verArbolServicio#tab_2" class=""  title="Volver a Servicio"  >
+            <a  href="/verArbolServicio#tab_2" class="btn btn-outline-info"  title="Volver a Servicio"  >
                 <span class="material-symbols-outlined">
                     reply_all
                 </span> VOLVER
@@ -36,26 +36,27 @@
             <div class="row">
                 <!-- datos agente -->
                 <div class="col-md-6">
-                    <div class="card card-default">
+                    <div class="card card-lightblue">
                         <div class="card-header">
                             <h3 class="card-title">
-                            <i class="fas fa-book"></i>
-                            Panel de Control - Actualizar Información Docente 
+                                <i class="fas fa-book mr-2"></i>
+                                Panel de Control - Actualizar Información Docente 
                             </h3>
                         </div>
                         <!-- /.card-header -->
                         <form method="POST" action="{{ route('formularioActualizarAgente') }}" class="formularioActualizarAgente">
                             @csrf
                             <div class="card-body">
-                                <div class="input-group">
-                                    <label for="Descripcion">Docente: </label>
-                                    <input type="text" class="form-control" id="DescripcionNombreAgenteActualizar" name="DescripcionNombreAgenteActualizar" placeholder="Ingrese Descripcion" value="{{$infoNodos[0]->Nombres}}">
-                                    <input type="hidden" name="idAgente"  id="idAgente" value="{{$infoNodos[0]->Agente}}">
-                                    <span class="input-group-append">
-                                        <a href="#modalAgente" class="btn btn-info btn-flat"  data-toggle="modal" data-placement="top" title="Agregar Docente"  data-target="#modalAgente">Agregar</a>
-                                        <a href="{{route('desvincularDocente',$infoNodos[0]->idNodo)}}" class="btn btn-danger btn-flat">Quitar</a>
-                                    </span>
-                                    
+                                <div class="form-group">
+                                    <label class="label-form" for="Descripcion">Docente: </label>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" id="DescripcionNombreAgenteActualizar" name="DescripcionNombreAgenteActualizar" placeholder="Ingrese Descripcion" value="{{$infoNodos[0]->Nombres}}">
+                                        <input type="hidden" name="idAgente"  id="idAgente" value="{{$infoNodos[0]->Agente}}">
+                                        <span class="input-group-append">
+                                            <a href="#modalAgente" class="btn btn-info btn-flat"  data-toggle="modal" data-placement="top" title="Agregar Docente"  data-target="#modalAgente">Agregar</a>
+                                            <a href="{{route('desvincularDocente',$infoNodos[0]->idNodo)}}" class="btn btn-danger btn-flat">Quitar</a>
+                                        </span>
+                                    </div>
                                 </div>
                                 <div class="form-group">
                                     <label for="Curso">Cargos / Función</label>
@@ -72,37 +73,37 @@
                                 <div class="form-group">
                                     <label for="EspCur">Espacio Curricular</label>
                                     <select class="form-control" name="EspCur" id="EspCur">
-                                    @foreach($EspaciosCurriculares as $key => $o)
-                                        @if ($o->idEspacioCurricular == $infoNodos[0]->EspacioCurricular)
-                                            <option value="{{$o->idEspacioCurricular}}" selected="selected">{{$o->Descripcion}}</option>
-                                        @else
-                                            <option value="{{$o->idEspacioCurricular}}">{{$o->Descripcion}}</option>
-                                        @endif
-                                    @endforeach
+                                        @foreach($EspaciosCurriculares as $key => $o)
+                                            @if ($o->idEspacioCurricular == $infoNodos[0]->EspacioCurricular)
+                                                <option value="{{$o->idEspacioCurricular}}" selected="selected">{{$o->Descripcion}}</option>
+                                            @else
+                                                <option value="{{$o->idEspacioCurricular}}">{{$o->Descripcion}}</option>
+                                            @endif
+                                        @endforeach
                                     </select>
                                 </div>
                                 <div class="form-group">
                                     <label for="SitRev">Situación de Revista</label>
                                     <select class="form-control" name="SitRev" id="SitRev">
-                                    @foreach($SituacionDeRevista as $key => $o)
-                                        @if ($o->idSituacionRevista == $infoNodos[0]->SitRev)
-                                            <option value="{{$o->idSituacionRevista}}" selected="selected">{{$o->Descripcion}}</option>
-                                        @else
-                                            <option value="{{$o->idSituacionRevista}}">{{$o->Descripcion}}</option>
-                                        @endif
-                                    @endforeach
+                                        @foreach($SituacionDeRevista as $key => $o)
+                                            @if ($o->idSituacionRevista == $infoNodos[0]->SitRev)
+                                                <option value="{{$o->idSituacionRevista}}" selected="selected">{{$o->Descripcion}}</option>
+                                            @else
+                                                <option value="{{$o->idSituacionRevista}}">{{$o->Descripcion}}</option>
+                                            @endif
+                                        @endforeach
                                     </select>
                                 </div>
                                 <div class="form-group">
                                     <label for="Division">Sala / Curso / División</label>
                                     <select class="form-control" name="Division" id="Division">
-                                    @foreach($Divisiones as $key => $o)
-                                        @if ($o->idDivision == $infoNodos[0]->Division)
-                                            <option value="{{$o->idDivision}}" selected="selected">{{$o->Descripcion}} - {{$o->DescripcionTurno}}</option>
-                                        @else
-                                            <option value="{{$o->idDivision}}">{{$o->Descripcion}} - {{$o->DescripcionTurno}}</option>
-                                        @endif
-                                    @endforeach
+                                        @foreach($Divisiones as $key => $o)
+                                            @if ($o->idDivision == $infoNodos[0]->Division)
+                                                <option value="{{$o->idDivision}}" selected="selected">{{$o->Descripcion}} - {{$o->DescripcionTurno}}</option>
+                                            @else
+                                                <option value="{{$o->idDivision}}">{{$o->Descripcion}} - {{$o->DescripcionTurno}}</option>
+                                            @endif
+                                        @endforeach
                                     </select>
                                 </div>
                                 <div class="form-group">
@@ -114,9 +115,9 @@
                                     <input type="date" class="form-control" id="FA" name="FA" placeholder="Ingrese Fecha de Alta" value="{{ \Carbon\Carbon::parse($infoNodos[0]->FechaDeAlta)->format('Y-m-d')}}">
                                     <input type="hidden" name="nodo" value="{{$infoNodos[0]->idNodo}}">
                                 </div>
-                                <div class="card-footer">
-                                    <button type="submit" class="btn btn-primary">Actualizar Informacion</button>
-                                </div>
+                            </div>
+                            <div class="card-footer bg-transparent d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary">Actualizar Informacion</button>
                             </div>
                         </form>
                         <!-- /.card-body -->
@@ -125,11 +126,11 @@
                 </div>
                 <!-- datos horario -->
                 <div class="col-md-6">
-                    <div class="card card-default">
+                    <div class="card card-lightblue">
                         <div class="card-header">
                             <h3 class="card-title">
-                            <i class="fas fa-book"></i>
-                            Panel de Control - Dias Disponibles y Horarios
+                                <i class="fas fa-book mr-2"></i>
+                                Panel de Control - Dias Disponibles y Horarios
                             </h3>
                         </div>
                         <!-- /.card-header -->
@@ -172,8 +173,6 @@
                                                         @endif
                                                     
                                                     @endforeach
-                                                        
-                                                    
                                                 </div>
                                             </div>
                                             <div class="form-group clearfix"></div>
@@ -192,7 +191,6 @@
                                                     <div class="icheck-success d-inline">
                                                         <input type="radio" name="r{{$o->idDiaSemana}}" value="SI" id="turnosx{{$o->idDiaSemana}}">
                                                         <label for="turnosx{{$o->idDiaSemana}}"><input type="text" class="form-control"  name="{{$o->Descripcion}}"></label>
-                                                        
                                                     </div>
                                                 </div>
                                             </div>
@@ -200,11 +198,10 @@
                                         </div> 
                                     @endif
                                  @endforeach
-                        
-                                <div class="card-footer">
-                                    <input type="hidden" name="Agn" id="Agn" value="{{$Nodo}}">
-                                    <button type="submit" class="btn btn-primary">Actualizar Informacion</button>
-                                </div>
+                            </div>
+                            <div class="card-footer bg-transparent d-flex justify-content-end">
+                                <input type="hidden" name="Agn" id="Agn" value="{{$Nodo}}">
+                                <button type="submit" class="btn btn-primary">Actualizar Informacion</button>
                             </div>
                         </form>
                         <!-- /.card-body -->
@@ -213,7 +210,7 @@
                 </div>
                 <!-- licencias -->
                 <div class="col-md-6">
-                    <div class="card card-default">
+                    <div class="card card-lightblue">
                         <div class="card-header">
                             <h3 class="card-title">
                             <i class="fas fa-book"></i>
@@ -225,11 +222,10 @@
                             @csrf
                             <div class="card-body">
                                 aqui todo licencias
-                        
-                                <div class="card-footer">
-                                    <input type="hidden" name="Agn" id="Agn" value="{{$Nodo}}">
-                                    <button type="submit" class="btn btn-primary">Actualizar Informacion</button>
-                                </div>
+                            </div>
+                            <div class="card-footer bg-transparent d-flex justify-content-end">
+                                <input type="hidden" name="Agn" id="Agn" value="{{$Nodo}}">
+                                <button type="submit" class="btn btn-primary">Actualizar Informacion</button>
                             </div>
                         </form>
                         <!-- /.card-body -->
@@ -245,53 +241,55 @@
 </section>
 
 <!--modales-->
-                    <div class="modal fade" id="modalAgente">
-                        <div class="modal-dialog modal-lg">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h4 class="modal-title">Buscar Agente</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+    <div class="modal fade" id="modalAgente">
+        <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h4 class="modal-title">Buscar Agente</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+            
+                <div class="card card-olive">
+                        <div class="card-header">
+                            <h3 class="card-title">Lista de Agentes: </h3>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="buscarAgente" placeholder="Ingrese DNI sin Puntos" value="">
+                                <button class="btn btn-info" type="button" id="traerAgentes" onclick="getAgentesActualizar()">buscar
+                                    <i class="fa fa-search"></i>
+                                </button>
                             </div>
-                            <div class="modal-body">
+                            <label>CUE:<b>{{ session('CUE') }}</b></label>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="examplex" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Apellido y Nombre</th>
+                                    <th>DNI</th>
+                                    <th>Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody id="contenidoAgentes">
                             
-                                <div class="card card-primary">
-                                        <div class="card-header">
-                                            <h3 class="card-title">Lista de Agentes: </h3>
-                                            <input type="text" class="form-control" id="buscarAgente" placeholder="Ingrese DNI sin Puntos" value="">
-                                            <button class="btn btn-success" type="button" id="traerAgentes" onclick="getAgentesActualizar()">buscar
-                                                <i class="fa fa-search"></i></button>
-                                        
-                                        <label>CUE:<b>{{ session('CUE') }}</b></label>
-                                        </div>
-                                        <!-- /.card-header -->
-                                        <div class="card-body">
-                                            <table id="examplex" class="table table-bordered table-striped">
-                                            <thead>
-                                                <tr>
-                                                    <th>ID</th>
-                                                    <th>Apellido y Nombre</th>
-                                                    <th>DNI</th>
-                                                    <th>Opciones</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody id="contenidoAgentes">
-                                            
-                                            </tbody>
-                                            </table>
-                                        </div>
-                                        <!-- /.card-body -->
-                                </div>
-                            </div>
-                            <div class="modal-footer justify-content-between">
-                                <button type="button" class="btn btn-primary"  data-dismiss="modal">Salir</button>
-                            </div>
+                            </tbody>
+                            </table>
                         </div>
-                        <!-- /.modal-content -->
-                        </div>
-                        <!-- /.modal-dialog -->
-                    </div>
+                        <!-- /.card-body -->
+                </div>
+            </div>
+            <div class="modal-footer justify-content-end">
+                <button type="button" class="btn btn-primary"  data-dismiss="modal">Salir</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
 
 
 @endsection
