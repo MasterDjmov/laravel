71a5a5eceed69fe0c48e38fcde74efbd2df05987
MasterDjmov <masterdjmov@gmail.com>
2023-04-18 18:31:36 -0300

    viendo login

--- app/Http/Controllers/BandejaController.php
+++ app/Http/Controllers/BandejaController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\Session\Session;
+//use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class BandejaController extends Controller
 {
     
-    public function index(){
-        return view('bandeja.index');
+    public function index(Request $request){
+        if ($request->session()->has('Usuario') == true) {
+            return view('bandeja.index');
+        }else{
+            Session::flush();
+            return redirect('/salir');
+
+        }
     }
 
     
     public function salir(){
         session(['Validar' => '']);
+        Session::flush();
          return redirect('/');
     }
 }
