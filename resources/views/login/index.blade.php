586441fa09990d38a98ff2dca3211fa2a35418bf
Leo Loyola <MasterDjmov>
2024-03-20 12:59:15 -0300

    bloqueo de login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -8,27 +8,40 @@
   <div class="lock-wrapper">
 
     <div id="time" style="color:rgb(74, 71, 71)"></div>
-
+    @php
+      $activo = "SI";
+    @endphp
 
     <div class="card text-center bg-op-1">
       <img src="{{ asset('img/seguridad.jpg');}}" alt="lock avatar"/>
       <div class="card-body">
         <h1><strong style="color:rgb(74, 71, 71)">Sistema <strong><strong style="color: rgb(75, 15, 15)">SAGE</strong></h1>
         <span class="locked">{{$mensajeError}}</span>
-        <form role="form" class="form-group" method="POST" action="{{ route('login') }}">
-          @csrf
-            <div class="form-group">
-              <input type="email" placeholder="Email" id="email" name="email" class="form-control mb-2">
-              <input type="password" placeholder="Password" id="password" name="clave" class="form-control">
-            </div>
+        @if ($activo=="SI")
+         
+              <div class="row" style="height:150px; width:100%; margin: 0 auto; position:relative">
+                <img src="{{ asset('img/mantenimiento.gif');}}" style="border: none; position: absolute; left:10px; top:10px">
+              </div>
+              <marquee style="color:red;font-size:24px;">Mantenimiento preventivo desde las 14 horas
+              </marquee>
+          
+        @else
+          <form role="form" class="form-group" method="POST" action="{{ route('login') }}">
+            @csrf
+              <div class="form-group">
+                <input type="email" placeholder="Email" id="email" name="email" class="form-control mb-2">
+                <input type="password" placeholder="Password" id="password" name="clave" class="form-control">
+              </div>
+                    
+              <div class="form-group">
+                <button class="btn btn-block btn-success btn-lg" type="submit">
+                    <i class="fa fa-arrow-right"></i>
+                </button>
+              </div>
                   
-            <div class="form-group">
-              <button class="btn btn-block btn-success btn-lg" type="submit">
-                  <i class="fa fa-arrow-right"></i>
-              </button>
-            </div>
-                
-        </form>
+          </form>
+        @endif
+       
       </div>
       
     </div>
