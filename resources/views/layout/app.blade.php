c2c2491771d48f62b5ad5c080e8358d83d634ad2
Leo Loyola <MasterDjmov>
2024-03-21 00:06:24 -0300

    resalto letras menu de admin

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -233,13 +233,13 @@
               <li class="nav-item">
                 <a href="{{route('buscar_dni_cue')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Consulta Temporal - Borrar</p>
+                  <p class="text-warning">Consulta Temporal - Borrar</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="{{route('limpiar_carga')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Borrar toda la carga</p>
+                  <p class="text-warning">Borrar toda la carga</p>
                 </a>
               </li>
             </ul>
